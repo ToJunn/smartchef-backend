1e7af55415a0ad2da7cfb41d59ec5daea5b43ebd
ToJunn <nqt303@2gmail.com>
2025-11-21 17:04:32 +0700

    update time token

--- config/settings.py
+++ config/settings.py
@@ -160,3 +160,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),   
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),    
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+}
